ad831e8c516bda0b29b9223d3575dc410f017dbe
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2024-11-28 11:14:30 +0500

    code update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this path based on your project structure
+    './public/index.html',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
